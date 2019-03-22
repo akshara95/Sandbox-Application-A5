d3f504d53235329492a2bc9dad03050c3c86b35a
akshara95 <perasani.akshara@gmail.com>
2019-03-22 12:15:59 -0700

    creating 1st component

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstPageComponent } from './components/first-page/first-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
